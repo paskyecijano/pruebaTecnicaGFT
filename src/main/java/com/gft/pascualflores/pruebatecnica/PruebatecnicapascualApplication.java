12976742a10d185909a17ffdc027cbf8c9e13a76
Pascual Flores Prada <paskyecijano@gmail.com>
2023-12-20 09:31:08 +0100

    Commit inicial

---
+++ src/main/java/com/gft/pascualflores/pruebatecnica/PruebatecnicapascualApplication.java
@@ -0,0 +1,13 @@
+package com.gft.pascualflores.pruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebatecnicapascualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebatecnicapascualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/pascualflores/pruebatecnica/PruebatecnicapascualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.pascualflores.pruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebatecnicapascualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
