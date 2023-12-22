81d41e0425be8cd0b7608228c10f62157dfd2385
Pascual Flores Prada <paskyecijano@gmail.com>
2023-12-22 13:14:09 +0100

    Rename camel case class

---
+++ src/main/java/com/gft/pascualflores/pruebatecnica/PruebaPascualApplication.java
@@ -0,0 +1,12 @@
+package com.gft.pascualflores.pruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPascualApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PruebaPascualApplication.class, args);
+  }
+}
---
+++ src/test/java/com/gft/pascualflores/pruebatecnica/PruebaPascualApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gft.pascualflores.pruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaPascualApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
