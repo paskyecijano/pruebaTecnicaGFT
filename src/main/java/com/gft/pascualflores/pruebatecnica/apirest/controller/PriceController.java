de17da267c8054ae5eb5596d799b156ea8bad296
Pascual Flores Prada <paskyecijano@gmail.com>
2023-12-26 13:24:24 +0100

    Cambio nombre endpoint

--- src/main/java/com/gft/pascualflores/pruebatecnica/apirest/controller/PriceController.java
+++ src/main/java/com/gft/pascualflores/pruebatecnica/apirest/controller/PriceController.java
@@ -1,7 +1,6 @@
 package com.gft.pascualflores.pruebatecnica.apirest.controller;
 
 import com.gft.pascualflores.pruebatecnica.apirest.data.ResponsePriceDto;
-import com.gft.pascualflores.pruebatecnica.apirest.exception.PriceNotFoundException;
 import com.gft.pascualflores.pruebatecnica.apirest.mapper.ApiMapper;
 import com.gft.pascualflores.pruebatecnica.domain.model.PriceDto;
 import com.gft.pascualflores.pruebatecnica.domain.usecase.GetPriceUseCase;
@@ -19,11 +18,10 @@ public class PriceController implements ApiApi {
   private final ApiMapper mapper;
 
   @Override
-  public ResponseEntity<ResponsePriceDto> _getPrice(
+  public ResponseEntity<ResponsePriceDto> _price(
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime date,
       Long productId,
-      Long brandId)
-      throws PriceNotFoundException {
+      Long brandId) {
     PriceDto priceDto =
         getPriceUseCase.getPriceByDateAndPriceIdAndBrandId(date, productId, brandId);
     return ResponseEntity.ok(this.mapper.priceDtoToResponsePriceDto(priceDto));
